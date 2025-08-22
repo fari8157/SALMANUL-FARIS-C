6fe2bc91334f38c3fdc166e46660ee598514b9ec
fari8157 <salmanul@gmail.com>
2025-08-22 17:10:01 +0530

    problem-4 solved

---
+++ Problem-4.js
@@ -0,0 +1,11 @@
+function countMultiples(arr) {
+  let result = {};
+  for (let i = 1; i <= 9; i++) {
+    result[i] = arr.filter(num => num % i === 0).length;
+  }
+  return result;
+}
+
+
+let input = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30];
+console.log(countMultiples(input));
